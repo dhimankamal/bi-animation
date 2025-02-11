38cadc94fe0b4e0285be3887193f3feb4a6246e6
anumeetkumar <anumeetkumar@gmail.com>
2025-02-11 14:00:11 +0530

    feat: added side text

--- script.js
+++ script.js
@@ -21,6 +21,10 @@ document.addEventListener("wheel", (event) => {
     isScrolling = false;
   }, 1200);
   let direction = event.deltaY > 0 ? 1 : -1;
+  if (order === 1) {
+    document.getElementById("right-text").style.opacity = "0";
+    document.getElementById("left-text").style.opacity = "0";
+  }
   if (direction === -1 && (order === 0 || order === 1)) {
     document.getElementById(`order1`).style.opacity = "1";
     document.getElementById(`order2`).style.opacity = "1";
@@ -28,7 +32,7 @@ document.addEventListener("wheel", (event) => {
       img.classList.remove("selected-img");
     });
   }
-  if (direction === -1 && order === 1) return order=order-1;
+  if (direction === -1 && order === 1) return (order = order - 1);
   if (direction === 1 && order === 4) return;
   if (order) {
     gsap.to(".scroll-container", {
@@ -40,6 +44,7 @@ document.addEventListener("wheel", (event) => {
       ease: "power1.inOut",
       onComplete: () => {},
     });
+
     document.getElementById(`order${order}`).style.opacity = "0.4";
     document.querySelectorAll(`#order${order} img`).forEach((img) => {
       img.classList.remove("selected-img");
@@ -59,4 +64,36 @@ document.addEventListener("wheel", (event) => {
     });
 
   order = direction === 1 ? order + 1 : order - 1;
+  if (order % 2 === 0) {
+    // even
+    gsap.to(`#left-text`, {
+      opacity: 1,
+      duration: 1,
+      onComplete: () => {},
+    });
+    gsap.to(`#right-text`, {
+      opacity: 0,
+      duration: 0.5,
+      onComplete: () => {},
+    });
+    document.getElementById(
+      "left-text"
+    ).src = `./assets/images/text-${order}.png`;
+  } else {
+    // odd
+    document.getElementById(
+      "right-text"
+    ).src = `./assets/images/text-${order}.png`;
+    gsap.to(`#left-text`, {
+      opacity: 0,
+      duration: 0.5,
+      onComplete: () => {},
+    });
+    gsap.to(`#right-text`, {
+      opacity: 1,
+      duration: 1,
+      onComplete: () => {},
+    });
+  }
+ 
 });
